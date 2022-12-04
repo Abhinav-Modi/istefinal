7135ffc5dc38ca9ee59a2a6e51c8ee4422dbe9ff
Abhinav-Modi <abhinavmodi07@gmail.com>
2022-12-04 15:18:17 +0530

    build

---
+++ Client/build/precache-manifest.a2b1c3e2c28c0c70387c3f90abf9ff9d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "be6f8f55bfb1e4158c0a",
+    "url": "/static/css/main.16fa831b.chunk.css"
+  },
+  {
+    "revision": "be6f8f55bfb1e4158c0a",
+    "url": "/static/js/main.be6f8f55.chunk.js"
+  },
+  {
+    "revision": "92819e1030022cca27a1",
+    "url": "/static/css/1.20f35934.chunk.css"
+  },
+  {
+    "revision": "92819e1030022cca27a1",
+    "url": "/static/js/1.92819e10.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3de27ba26e678f3aac760fd1afffe272",
+    "url": "/static/media/web.3de27ba2.svg"
+  },
+  {
+    "revision": "73be98c419c29059d0e22738b9ecaba4",
+    "url": "/static/media/app.73be98c4.svg"
+  },
+  {
+    "revision": "941b137eb6b386e07dd8a4d4594e25a4",
+    "url": "/static/media/graphic.941b137e.svg"
+  },
+  {
+    "revision": "f0d718396b17a04c4206992f9bd09cd4",
+    "url": "/static/media/content.f0d71839.svg"
+  },
+  {
+    "revision": "d8886ec2272bf3d28deca7f65e58a45e",
+    "url": "/static/media/hr.d8886ec2.svg"
+  },
+  {
+    "revision": "161ea29840ef7deae2db17e523002cd9",
+    "url": "/static/media/logo.161ea298.svg"
+  },
+  {
+    "revision": "5090048c7247d1c90a5eaa6d99f9f6e4",
+    "url": "/static/media/aib1.5090048c.jpeg"
+  },
+  {
+    "revision": "7cc81c5332278d8a8df4e7d0d2dfafc6",
+    "url": "/static/media/ts.7cc81c53.jpeg"
+  },
+  {
+    "revision": "a458f525907c80efe0c7c68e0c19cd84",
+    "url": "/static/media/ts2.a458f525.jpg"
+  },
+  {
+    "revision": "d9cd88f6e94dafd3d0eb33d402123dcb",
+    "url": "/static/media/acumen.d9cd88f6.svg"
+  },
+  {
+    "revision": "01a178ed8acdd9f64cc9b5dbae876ad7",
+    "url": "/static/media/oldlogo.01a178ed.svg"
+  },
+  {
+    "revision": "25ea70db6ab70b542fff4452ce897724",
+    "url": "/static/media/manipal.25ea70db.png"
+  },
+  {
+    "revision": "6c96dcfd9c649432d4e1fa5ca0e60113",
+    "url": "/static/media/abhinav.6c96dcfd.jpeg"
+  },
+  {
+    "revision": "13a503e924c74375ac6a22d3e47ab003",
+    "url": "/index.html"
+  }
+];
